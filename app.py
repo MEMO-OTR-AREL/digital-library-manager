516bca4810f6117d120404625496625dcd576752
Mehmet Ali Öter <memoliotr.47@gmail.com>
2026-05-12 01:32:42 +0300

    [US1] Added user registration system

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 
+def create_database():
+    connection = sqlite3.connect("library.db")
+
+    with open("schema.sql", "r") as file:
+        connection.executescript(file.read())
+
+    connection.close()
+
+
 @app.route("/")
 def home():
-    return "Digital Library Manager is Running!"
+    return render_template("home.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        connection = sqlite3.connect("library.db")
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+            (username, email, password)
+        )
+
+        connection.commit()
+        connection.close()
+
+        return "User registered successfully!"
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
