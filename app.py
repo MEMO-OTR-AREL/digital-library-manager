9fc03bd2c4e9f6b8942921ea3b26c0c1373f46d0
Mehmet Ali Öter <memoliotr.47@gmail.com>
2026-05-13 03:18:31 +0300

    [US1] Updated website design converted project language

--- app.py
+++ app.py
@@ -5,19 +5,15 @@ app = Flask(__name__)
 app.secret_key = "secret123"
 
 
-def create_database():
-    connection = sqlite3.connect("library.db")
-
-    with open("schema.sql", "r") as file:
-        connection.executescript(file.read())
-
-    connection.close()
-
+# HOME
 
 @app.route("/")
 def home():
+
     return render_template("home.html")
 
+
+# REGISTER
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -32,17 +28,28 @@ def register():
         cursor = connection.cursor()
 
         cursor.execute(
-            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+            """
+            INSERT INTO users
             (username, email, password)
+
+            VALUES (?, ?, ?)
+            """,
+            (
+                username,
+                email,
+                password
+            )
         )
 
         connection.commit()
         connection.close()
 
-        return "User registered successfully!"
+        return redirect("/login")
 
     return render_template("register.html")
 
+
+# LOGIN
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -56,8 +63,15 @@ def login():
         cursor = connection.cursor()
 
         cursor.execute(
-            "SELECT * FROM users WHERE username = ? AND password = ?",
-            (username, password)
+            """
+            SELECT *
+            FROM users
+            WHERE username = ? AND password = ?
+            """,
+            (
+                username,
+                password
+            )
         )
 
         user = cursor.fetchone()
@@ -65,15 +79,21 @@ def login():
         connection.close()
 
         if user:
+
             session["user_id"] = user[0]
             session["username"] = user[1]
 
             return redirect("/dashboard")
 
-        return "Invalid username or password"
+        return render_template(
+            "login.html",
+            error="Invalid username or password"
+        )
 
     return render_template("login.html")
 
+
+# DASHBOARD
 
 @app.route("/dashboard")
 def dashboard():
@@ -84,43 +104,96 @@ def dashboard():
     connection = sqlite3.connect("library.db")
     cursor = connection.cursor()
 
+    # TOTAL BOOKS
+
     cursor.execute(
-        "SELECT COUNT(*) FROM books WHERE user_id = ?",
+        """
+        SELECT COUNT(*)
+        FROM books
+        WHERE user_id = ?
+        """,
         (session["user_id"],)
     )
 
     total_books = cursor.fetchone()[0]
 
+    # READING BOOKS
+
     cursor.execute(
         """
-        SELECT COUNT(*) FROM books
-        WHERE user_id = ? AND status = 'Reading'
+        SELECT COUNT(*)
+        FROM books
+        WHERE user_id = ?
+        AND status = 'Reading'
         """,
         (session["user_id"],)
     )
 
     reading_books = cursor.fetchone()[0]
 
+    # COMPLETED BOOKS
+
     cursor.execute(
         """
-        SELECT COUNT(*) FROM books
-        WHERE user_id = ? AND status = 'Completed'
+        SELECT COUNT(*)
+        FROM books
+        WHERE user_id = ?
+        AND status = 'Completed'
         """,
         (session["user_id"],)
     )
 
     completed_books = cursor.fetchone()[0]
 
+    # FAVORITE BOOKS
+
+    cursor.execute(
+        """
+        SELECT COUNT(*)
+        FROM books
+        WHERE user_id = ?
+        AND favorite = 1
+        """,
+        (session["user_id"],)
+    )
+
+    favorite_books = cursor.fetchone()[0]
+
+    # RECENT BOOKS
+
+    cursor.execute(
+        """
+        SELECT *
+        FROM books
+        WHERE user_id = ?
+        ORDER BY created_at DESC
+        LIMIT 3
+        """,
+        (session["user_id"],)
+    )
+
+    recent_books = cursor.fetchall()
+
     connection.close()
 
     return render_template(
         "dashboard.html",
+
         username=session["username"],
+
         total_books=total_books,
+
         reading_books=reading_books,
-        completed_books=completed_books
+
+        completed_books=completed_books,
+
+        favorite_books=favorite_books,
+
+        recent_books=recent_books
     )
 
+
+# ADD BOOK
 
 @app.route("/add_book", methods=["GET", "POST"])
 def add_book():
@@ -137,14 +210,65 @@ def add_book():
         rating = request.form["rating"]
         progress = request.form["progress"]
 
+        # BOOK COVERS
+
+        cover_map = {
+
+            "Suç ve Ceza":
+                "/static/images/suc_ve_ceza.jpg",
+
+            "Kumarbaz":
+                "/static/images/kumarbaz.jpg",
+
+            "Sefiller":
+                "/static/images/sefiller.jpg",
+
+            "Clean Code":
+                "/static/images/clean_code.jpg",
+
+            "Python Crash Course":
+                "/static/images/python_crash_course.jpg",
+
+            "Atomic Habits":
+                "/static/images/atomic_habits.jpg",
+
+            "Thinking Fast and Slow":
+                "/static/images/thinking_fast_and_slow.jpg",
+
+            "Harry Potter":
+                "/static/images/harry_potter.jpg",
+
+            "Hobbit":
+                "/static/images/hobbit.jpg",
+
+            "Dune":
+                "/static/images/dune.jpg"
+        }
+
+        cover_url = cover_map.get(
+            title,
+            "/static/images/suc_ve_ceza.jpg"
+        )
+
         connection = sqlite3.connect("library.db")
         cursor = connection.cursor()
 
         cursor.execute(
             """
             INSERT INTO books
-            (user_id, title, author, category, status, rating, progress)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
+            (
+                user_id,
+                title,
+                author,
+                category,
+                status,
+                rating,
+                progress,
+                favorite,
+                cover_url
+            )
+
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             """,
             (
                 session["user_id"],
@@ -153,17 +277,21 @@ def add_book():
                 category,
                 status,
                 rating,
-                progress
+                progress,
+                0,
+                cover_url
             )
         )
 
         connection.commit()
         connection.close()
 
-        return "Book added successfully!"
+        return redirect("/books")
 
     return render_template("add_book.html")
 
+
+# BOOKS
 
 @app.route("/books")
 def books():
@@ -178,18 +306,23 @@ def books():
     cursor = connection.cursor()
 
     query = """
-        SELECT * FROM books
+        SELECT *
+        FROM books
         WHERE user_id = ?
     """
 
     parameters = [session["user_id"]]
 
     if search:
+
         query += " AND title LIKE ?"
+
         parameters.append(f"%{search}%")
 
     if category:
+
         query += " AND category = ?"
+
         parameters.append(category)
 
     cursor.execute(query, parameters)
@@ -204,6 +337,8 @@ def books():
     )
 
 
+# DELETE BOOK
+
 @app.route("/delete_book/<int:book_id>")
 def delete_book(book_id):
 
@@ -214,8 +349,15 @@ def delete_book(book_id):
     cursor = connection.cursor()
 
     cursor.execute(
-        "DELETE FROM books WHERE id = ? AND user_id = ?",
-        (book_id, session["user_id"])
+        """
+        DELETE FROM books
+        WHERE id = ?
+        AND user_id = ?
+        """,
+        (
+            book_id,
+            session["user_id"]
+        )
     )
 
     connection.commit()
@@ -223,6 +365,8 @@ def delete_book(book_id):
 
     return redirect("/books")
 
+
+# FAVORITE BOOK
 
 @app.route("/favorite_book/<int:book_id>")
 def favorite_book(book_id):
@@ -237,9 +381,13 @@ def favorite_book(book_id):
         """
         UPDATE books
         SET favorite = 1
-        WHERE id = ? AND user_id = ?
+        WHERE id = ?
+        AND user_id = ?
         """,
-        (book_id, session["user_id"])
+        (
+            book_id,
+            session["user_id"]
+        )
     )
 
     connection.commit()
@@ -247,6 +395,8 @@ def favorite_book(book_id):
 
     return redirect("/books")
 
+
+# EDIT BOOK
 
 @app.route("/edit_book/<int:book_id>", methods=["GET", "POST"])
 def edit_book(book_id):
@@ -269,8 +419,17 @@ def edit_book(book_id):
         cursor.execute(
             """
             UPDATE books
-            SET title = ?, author = ?, category = ?, status = ?, rating = ?, progress = ?
-            WHERE id = ? AND user_id = ?
+
+            SET
+                title = ?,
+                author = ?,
+                category = ?,
+                status = ?,
+                rating = ?,
+                progress = ?
+
+            WHERE id = ?
+            AND user_id = ?
             """,
             (
                 title,
@@ -290,16 +449,29 @@ def edit_book(book_id):
         return redirect("/books")
 
     cursor.execute(
-        "SELECT * FROM books WHERE id = ? AND user_id = ?",
-        (book_id, session["user_id"])
+        """
+        SELECT *
+        FROM books
+        WHERE id = ?
+        AND user_id = ?
+        """,
+        (
+            book_id,
+            session["user_id"]
+        )
     )
 
     book = cursor.fetchone()
 
     connection.close()
 
-    return render_template("edit_book.html", book=book)
+    return render_template(
+        "edit_book.html",
+        book=book
+    )
 
+
+# LOGOUT
 
 @app.route("/logout")
 def logout():
@@ -310,4 +482,6 @@ def logout():
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
+
--- schema.sql
+++ schema.sql
@@ -1,21 +1,38 @@
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+
     username TEXT NOT NULL UNIQUE,
+
     email TEXT NOT NULL,
+
     password TEXT NOT NULL
 );
 
 CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+
     user_id INTEGER NOT NULL,
+
     title TEXT NOT NULL,
+
     author TEXT NOT NULL,
+
     category TEXT,
+
     status TEXT,
+
     rating INTEGER,
-    progress INTEGER
+
+    progress INTEGER,
+
     favorite INTEGER DEFAULT 0,
-    access_until TEXT,
-    created_at TEXT,
+
+    cover_url TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
